b167d207586d2fe36969d9fda1cb0ee1ded78a09
yann <yannlefranc330@gmail.com>
2023-04-13 18:41:28 +0200

    the witcher

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // creéation d'un petit programme.
+        String name = "the witcher";
+
+        // Code here can use x
+        System.out.println("the witcher");
+    }
+}
+
+
+
+
+
+
+
+
+
+
